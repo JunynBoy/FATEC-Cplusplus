da7db0cc933ceac372773388864f72f423a5d3c8
Marcos Gasparini <103968839+JunynBoy@users.noreply.github.com>
2022-11-12 12:04:20 -0300

    att

--- Fatec C++/Exercícios em aula/Parte 6/ex002.cpp
+++ Fatec C++/Exercícios em aula/Parte 6/ex002.cpp
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 //2-Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado dos componentes deste vetor,
 // armazenando o resultado em outro vetor. Os conjuntos têm 10 elementos cada. Imprimir todos os conjuntos.
 int main(){
@@ -16,12 +17,12 @@ int main(){
 	}
 	
 	for (i=1;i<=10;i++){
-		v2[i]==v1[i]^2;
+		v2[i]=v1[i]*v1[i];
 		printf("O valor digitado no %i° campo foi = %.1f\n", i,v1[i]);
-		printf("O valor digitado no %i° campo ao quadrado é = %f\n", i,v2[i]);
+		printf("O valor digitado no %i° campo ao quadrado é = %.1f\n", i,v2[i]);
 	}
 	
 	
-	
+	system("PAUSE");
 	return 0;
 }
